849a7052cbef6ed4741cf7ee626570a232046ce7
annonymususer6 <annonymususer6@gmail.com>
2023-10-13 22:52:13 +0530

    final Code

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+const merchants = new Map();
+const isActive = {
+    name: null,
+    key: null
+};
 
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'merchant.html');
+    res.sendFile(filePath);
+});
 app.post('/create_order', async (req, res) => {
     try {
+        if (!isActive.name || merchants.size === 0) {
+            return res.status(500).send('internal server error');
+        }
+
         const {
-            key,
             client_txn_id,
             amount,
-            p_info,
-            customer_name,
-            customer_email,
-            customer_mobile,
-            redirect_url
         } = req.body;
 
-        if (!key || !client_txn_id || !amount || !p_info || !customer_name || !customer_email || !customer_mobile || !redirect_url) {
+        if (!client_txn_id || !amount) {
             return res.status(400).json({ error: 'Missing required parameters' });
         }
 
         const payload = {
-            key,
-            client_txn_id,
-            amount,
-            p_info,
-            customer_name,
-            customer_email,
-            customer_mobile,
-            redirect_url
+            key: isActive.key,
+            client_txn_id: client_txn_id,
+            amount: amount,
+            p_info: 'abc',
+            customer_name: 'abc',
+            customer_email: 'abc@gmail.com',
+            customer_mobile: "1234567890",
+            redirect_url: 'https://google.com'
         };
 
         const response = await axios.post('https://api.ekqr.in/api/create_order', payload);
@@ -51,36 +60,50 @@ app.post('/create_order', async (req, res) => {
     }
 });
 
-
-app.post('/check_order_status', async (req, res) => {
-    try {
-
-        const { key, client_txn_id, txn_date } = req.body;
-
-
-        if (!key || !client_txn_id || !txn_date) {
-            return res.status(400).json({ error: 'Missing required parameters' });
-        }
-
-
-        const payload = {
-            key,
-            client_txn_id,
-            txn_date
-        };
-
-
-        const response = await axios.post('https://api.ekqr.in/api/check_order_status', payload);
-
-
-        res.status(response.status).json(response.data);
-    } catch (error) {
-        console.error('Error:', error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
+app.post('/add_merchant', (req, res) => {
+    const { name, key } = req.body;
+    if (!isActive.name) {
+        isActive.name = name;
+        isActive.key = key;
     }
+
+    merchants.set(name, { name: name, key: key });
+
+    res.status(200).send();
 });
 
+app.post('/activate_merchant', (req, res) => {
+    const { name } = req.body;
+
+    if (!merchants.has(name)) return res.status(400).send('no such merchant');
+
+    const merchant = merchants.get(name);
+    isActive.name = merchant.name;
+    isActive.key = merchant.key;
+
+    return res.status(200).send(isActive.name + ' enabled');
+});
+
+app.get('/get_merchants', (req, res) => {
+    if (merchants.size === 0) return res.status(400).json([]);
+
+    let data = [];
+    merchants.forEach((merchant) => {
+        data.push({ name: merchant.name, key: merchant.key, status: (isActive.name === merchant.name) });
+    });
+
+    res.status(200).json(data);
+});
+
+app.post('/remove_merchant', (req, res) => {
+    const { name } = req.body;
+
+    merchants.delete(name);
+
+    res.status(200);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
